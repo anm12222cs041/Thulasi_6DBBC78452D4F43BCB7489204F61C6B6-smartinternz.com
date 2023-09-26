b271bc2affb230c03251a3acb7395fca42590cdd
anm12222cs041 <144452699+anm12222cs041@users.noreply.github.com>
2023-09-26 12:30:33 +0530

    Update main2.py.py

--- challaenge unit 3/main2.py.py
+++ challaenge unit 3/main2.py.py
@@ -8,10 +8,10 @@ def sort_students(student_list):
     sorted_students = sorted(student_list, key=lambda x: x.cgpa, reverse=True)
     return sorted_students
 students1 = [
-    Student("bavithra", "S101", 6.8),
-    Student("arthi", "D102", 5.6),
-    Student("Gobi", "G103", 4.4),
-    Student("gobika", "B104", 3.3),
+    Student("ram", "S101", 6.8),
+    Student("thulasi", "D102", 5.6),
+    Student("clpmam", "G103", 4.4),
+    Student("athi", "B104", 3.3),
 ]
 
 sorted_students1 = sort_students(students1)
